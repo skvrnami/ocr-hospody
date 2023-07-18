85829bec32fb393ccbdfa606fe43ca924aa540ab
Michael Škvrňák <michael.skvrnak@protonmail.com>
2023-07-18 22:24:02 +0200

    Add further sources

--- 00_scrape_imgs.R
+++ 00_scrape_imgs.R
@@ -1,46 +1,86 @@
 library(rvest)
 
-book_id <- 301
-dir.create(glue::glue("output/{book_id}"))
-for(i in 1:492){
-    cat(i)
-    page_no <- i
-    url <- glue::glue("https://sources.cms.flu.cas.cz/src/index.php?s=v&cat=7&bookid={book_id}&page={page_no}")
-    html <- read_html(url)
-    html %>% 
-        html_node("img") %>% 
-        html_attr("src") %>% 
-        paste0("https://sources.cms.flu.cas.cz/src/", .) %>% 
-        download.file(., destfile = glue::glue("output/{book_id}/{page_no}.jpg"))
-    Sys.sleep(1)
+# TODO: replace base_url with cat
+#' @param book_id Id of book inferred from the URL
+#' @param end_page Last page to be scraped, number determined from the URL
+#' @param start_page Start page to scrape from (default to 1), useful when 
+#' the for loop breaks because of poor internet connection etc.
+#' @param base_url base URL
+download_book_imgs <- function(book_id, end_page, start_page = 1, base_url){
+    if(!dir.exists(glue::glue("output/{book_id}"))){
+        dir.create(glue::glue("output/{book_id}"))   
+    }
+    for(i in start_page:end_page){
+        cat(i)
+        page_no <- i
+        url <- glue::glue("{base_url}={book_id}&page={page_no}")
+        html <- read_html(url)
+        html %>% 
+            html_node("img") %>% 
+            html_attr("src") %>% 
+            paste0("https://sources.cms.flu.cas.cz/src/", .) %>% 
+            download.file(., destfile = glue::glue("output/{book_id}/{page_no}.jpg"))
+        Sys.sleep(1)
+    }
 }
 
-book_id <- 171
-dir.create(glue::glue("output/{book_id}"))
-for(i in 1:507){
-    cat(i)
-    page_no <- i
-    url <- glue::glue("https://sources.cms.flu.cas.cz/src/index.php?s=v&cat=7&bookid={book_id}&page={page_no}")
-    html <- read_html(url)
-    html %>% 
-        html_node("img") %>% 
-        html_attr("src") %>% 
-        paste0("https://sources.cms.flu.cas.cz/src/", .) %>% 
-        download.file(., destfile = glue::glue("output/{book_id}/{page_no}.jpg"))
-    Sys.sleep(1)
-}
+# Codex diplomaticus Bohemiae
+## CDB I
+download_book_imgs(book_id = 1244, end_page = 579, 
+                   base_url = "https://sources.cms.flu.cas.cz/src/index.php?s=v&cat=2&bookid")
+## CDB II
+download_book_imgs(book_id = 1243, end_page = 585,
+                   base_url = "https://sources.cms.flu.cas.cz/src/index.php?s=v&cat=2&bookid")
+## CDB III/1
+download_book_imgs(book_id = 565, end_page = 272, 
+                   base_url = "https://sources.cms.flu.cas.cz/src/index.php?s=v&cat=2&bookid")
+## CDB III/2
+download_book_imgs(book_id = 598, end_page = 125, 
+                   base_url = "https://sources.cms.flu.cas.cz/src/index.php?s=v&cat=2&bookid")
 
-book_id <- 154
-dir.create(glue::glue("output/{book_id}"))
-for(i in 1:449){
-    cat(i)
-    page_no <- i
-    url <- glue::glue("https://sources.cms.flu.cas.cz/src/index.php?s=v&cat=41&bookid={book_id}&page={page_no}")
-    html <- read_html(url)
-    html %>% 
-        html_node("img") %>% 
-        html_attr("src") %>% 
-        paste0("https://sources.cms.flu.cas.cz/src/", .) %>% 
-        download.file(., destfile = glue::glue("output/{book_id}/{page_no}.jpg"))
-    Sys.sleep(1)
-}
+# Regesta Bohemiae et Moraviae
+## RBM I
+download_book_imgs(book_id = 222, end_page = 819, 
+                   base_url = "https://sources.cms.flu.cas.cz/src/index.php?s=v&cat=8&bookid")
+## RBM II
+download_book_imgs(book_id = 132, end_page = 1495, 
+                   base_url = "https://sources.cms.flu.cas.cz/src/index.php?s=v&cat=8&bookid")
+## RBM III
+download_book_imgs(book_id = 130, end_page = 962, 
+                   base_url = "https://sources.cms.flu.cas.cz/src/index.php?s=v&cat=8&bookid")
+## RBM IV
+download_book_imgs(book_id = 196, end_page = 1020, 
+                   base_url = "https://sources.cms.flu.cas.cz/src/index.php?s=v&cat=8&bookid")
+## RBM VI/1
+download_book_imgs(book_id = 698, end_page = 200, 
+                   base_url = "https://sources.cms.flu.cas.cz/src/index.php?s=v&cat=8&bookid")
+## RBM VI/2
+download_book_imgs(book_id = 699, end_page = 357, 
+                   base_url = "https://sources.cms.flu.cas.cz/src/index.php?s=v&cat=8&bookid")
+## RBM VII/1
+download_book_imgs(book_id = 1061, end_page = 193, 
+                   base_url = "https://sources.cms.flu.cas.cz/src/index.php?s=v&cat=8&bookid")
+## RBM VII/2
+download_book_imgs(book_id = 1060, end_page = 209, 
+                   base_url = "https://sources.cms.flu.cas.cz/src/index.php?s=v&cat=8&bookid")
+## RBM VII/3
+download_book_imgs(book_id = 1062, end_page = 193, 
+                   base_url = "https://sources.cms.flu.cas.cz/src/index.php?s=v&cat=8&bookid")
+## RBM VII/4
+download_book_imgs(book_id = 1063, end_page = 169, 
+                   base_url = "https://sources.cms.flu.cas.cz/src/index.php?s=v&cat=8&bookid")
+## RBM VII/5
+download_book_imgs(book_id = 1064, end_page = 163, 
+                   base_url = "https://sources.cms.flu.cas.cz/src/index.php?s=v&cat=8&bookid")
+
+
+# Die Landtafel des Markgrafthumes Mähren. Brünner Cuda
+download_book_imgs(book_id = 171, end_page = 507, 
+                   base_url = "https://sources.cms.flu.cas.cz/src/index.php?s=v&cat=7&bookid")
+# Die Landtafel des Markgrafthumes Mähren. Olmützer Cuda
+download_book_imgs(book_id = 301, end_page = 492, 
+                   base_url = "https://sources.cms.flu.cas.cz/src/index.php?s=v&cat=7&bookid")
+# Decem registra censuum
+download_book_imgs(book_id = 154, end_page = 449, 
+                   base_url = "https://sources.cms.flu.cas.cz/src/index.php?s=v&cat=41&bookid")
+
--- 01_ocr.R
+++ 01_ocr.R
@@ -2,40 +2,70 @@ library(dplyr)
 library(tesseract)
 
 # tesseract_download("lat")
-
-files_301 <- list.files("output/301/", pattern = "*.jpg", full.names = TRUE)
-files_171 <- list.files("output/171/", pattern = "*.jpg", full.names = TRUE)
-files_154 <- list.files("output/154/", pattern = "*.jpg", full.names = TRUE)
-
 lat <- tesseract("lat", options = list("tessedit_pageseg_mode"=1))
 
-for(i in seq_along(files_154)){
-    cat(i, "\n")
-    try({
-        page_id <- stringr::str_extract(files_154[i], "[0-9]+.jpg") %>% 
-            gsub(".jpg", "", .)
-        text <- ocr(files_154[i], engine = lat)    
-        writeLines(text, glue::glue("texts/154/{page_id}.txt"))  
-    })
+find_files <- function(path, pattern = "*.jpg"){
+    list.files(
+        path = path, 
+        pattern = pattern, 
+        full.names = TRUE
+    )
 }
 
-for(i in seq_along(files_301)){
-    cat(i, "\n")
-    try({
-        page_id <- stringr::str_extract(files_301[i], "[0-9]+.jpg") %>% 
-            gsub(".jpg", "", .)
-        text <- ocr(files_301[i], engine = lat)    
-        writeLines(text, glue::glue("texts/301/{page_id}.txt"))  
-    })
+files_301 <- find_files("output/301/")
+files_171 <- find_files("output/171/")
+files_154 <- find_files("output/154/")
+
+files_130 <- find_files("output/130/")
+files_132 <- find_files("output/132/")
+files_154 <- find_files("output/154/")
+files_196 <- find_files("output/196/")
+files_222 <- find_files("output/222/")
+files_565 <- find_files("output/565/")
+files_598 <- find_files("output/598/")
+files_698 <- find_files("output/698/")
+files_699 <- find_files("output/699/")
+files_1060 <- find_files("output/1060/")
+files_1061 <- find_files("output/1061/")
+files_1062 <- find_files("output/1062/")
+files_1063 <- find_files("output/1063/")
+files_1064 <- find_files("output/1064/")
+files_1243 <- find_files("output/1243/")
+files_1244 <- find_files("output/1244/")
+
+ocr_files <- function(files, path, engine = lat){
+    if(!dir.exists(path)){
+        dir.create(path)
+    }
+    for(i in seq_along(files)){
+        cat(i, "\n")
+        try({
+            page_id <- stringr::str_extract(files[i], "[0-9]+.jpg") %>% 
+                gsub(".jpg", "", .)
+            text <- ocr(files[i], engine = engine)
+            writeLines(text, glue::glue("{path}/{page_id}.txt"))
+        })
+    }
 }
 
-for(i in seq_along(files_171)){
-    cat(i, "\n")
-    try({
-        page_id <- stringr::str_extract(files_171[i], "[0-9]+.jpg") %>% 
-            gsub(".jpg", "", .)
-        text <- ocr(files_171[i], engine = lat)    
-        writeLines(text, glue::glue("texts/171/{page_id}.txt"))  
-    })
-}
+ocr_files(files_154, "texts/154")
+ocr_files(files_171, "texts/171")
+ocr_files(files_301, "texts/301")
+
+ocr_files(files_130, "texts/130")
+ocr_files(files_132, "texts/132")
+ocr_files(files_154, "texts/154")
+ocr_files(files_196, "texts/196")
+ocr_files(files_222, "texts/222")
+ocr_files(files_565, "texts/565")
+ocr_files(files_598, "texts/598")
+ocr_files(files_698, "texts/698")
+ocr_files(files_699, "texts/699")
+ocr_files(files_1060, "texts/1060")
+ocr_files(files_1061, "texts/1061")
+ocr_files(files_1062, "texts/1062")
+ocr_files(files_1063, "texts/1063")
+ocr_files(files_1064, "texts/1064")
+ocr_files(files_1243, "texts/1243")
+ocr_files(files_1244, "texts/1244")
 
